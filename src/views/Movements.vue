adf1a8b48dd134e09d7784bdf66c72f50d4e6ec3
VickyMastro <vicmastroianni1702@gmail.com>
2022-09-06 20:28:30 -0300

    Movements view

---
+++ src/views/Movements.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-button
+      class="text-decoration-none"
+      variant="link"
+      @click="changeView('ShowIncomes')"
+    >
+      Ingresos
+    </b-button>
+    <b-button
+      class="text-decoration-none"
+      variant="link"
+      @click="changeView('ShowEgress')"
+    >
+      Egresos
+    </b-button>
+    <component :is="componentToShow"></component>
+  </div>
+</template>
+
+<script>
+import ShowIncomes from "../components/ShowIncomes.vue";
+import ShowEgress from "../components/ShowEgress.vue";
+
+export default {
+  name: "Movements",
+  data() {
+    return {
+      componentToShow: "ShowIncomes",
+    };
+  },
+  methods: {
+    changeView(componentName) {
+        this.componentToShow = componentName
+    },
+  },
+  components: {
+    ShowIncomes,
+    ShowEgress,
+  },
+};
+</script>
