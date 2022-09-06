bbb24e560d08a5ed56d93b9c88b128fa87f12683
VickyMastro <vicmastroianni1702@gmail.com>
2022-09-06 20:23:09 -0300

    use vue-fab

---
+++ src/components/CreateButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <fab
+    :position="position"
+    :bg-color="bgColor"
+    :actions="fabActions"
+    @income="addIncome"
+    @agress="addEgress"
+  >
+  </fab>
+</template>
+
+<script>
+import fab from "vue-fab";
+export default {
+  name: "CreateSpending",
+  data() {
+    return {
+      bgColor: "#778899",
+      position: "bottom-right",
+      fabActions: [
+        {
+          name: "income",
+          icon: "cached",
+        },
+        {
+          name: "agress",
+          icon: "add_alert",
+        },
+      ],
+    };
+  },
+  methods: {
+    addIncome() {
+      console.log("ingreso");
+    },
+    addEgress() {
+      console.log("egreso");
+      this.$router.push("/createSpending")
+    },
+  },
+  components: {
+    fab,
+  },
+};
+</script>
