0141522565730363beb6584a8ee5e1678b490612
VickyMastro <vicmastroianni1702@gmail.com>
2022-09-30 16:17:13 -0300

    add form to income

---
+++ src/components/Incomes/FormIncome.vue
@@ -0,0 +1,76 @@
+<template>
+  <b-container fluid class="mt-4">
+    Crear ingreso
+    <!-- fecha del ingreso -->
+    <b-row class="mt-4" align-v="center">
+      <b-col sm="3">
+        <label>Fecha:</label>
+      </b-col>
+      <b-col sm="5">
+        <b-form-input
+          type="date"
+          :value="incomeData.date"
+          @input="(value) => $emit('setValue', 'date', value)"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+
+    <!-- Titulo -->
+    <b-row class="mt-4" align-v="center">
+      <b-col sm="3">
+        <label>Titulo:</label>
+      </b-col>
+      <b-col sm="9">
+        <b-form-input
+          :value="incomeData.title"
+          @input="(value) => $emit('setValue', 'title', value)"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+
+    <!-- Cuenta a la que va el ingreso -->
+    <b-row class="mt-4" align-v="center">
+      <b-col sm="3">
+        <label>Cuenta:</label>
+      </b-col>
+      <b-col sm="3">
+        <b-form-select
+          :options="options"
+          :value="incomeData.account_id"
+          @input="(value) => $emit('setValue', 'account_id', value)"
+        ></b-form-select>
+      </b-col>
+
+      <!-- Monto del ingreso -->
+      <b-col sm="3">
+        <label>Monto:</label>
+      </b-col>
+      <b-col sm="3">
+        <b-form-input
+          type="number"
+          :value="incomeData.cash"
+          @input="(value) => $emit('setValue', 'cash', value)"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+
+    <!-- Boton -->
+    <slot></slot>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "FormIncome",
+  props: ["incomeData"],
+  data() {
+    return {
+      options: [
+        { value: "a", text: "Mercado Pago" },
+        { value: "b", text: "BBVA" },
+        { value: "c", text: "Billetera" },
+      ],
+    };
+  },
+};
+</script>
