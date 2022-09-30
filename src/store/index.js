e74953336c436337fe21f21498bbdd4637dee536
VickyMastro <vicmastroianni1702@gmail.com>
2022-09-30 11:40:31 -0300

    conect the store with supabase

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,47 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { supabaseClient } from "@/supabase/Client.js";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    userData: null,
   },
+
+  getters: {
+    getIncomes(state) {
+      return state.userDataIncome;
+    },
+
+    getUser(state) {
+      return state.userData;
+    },
+
+    getFullNameUser(state) {
+      return state.userData.user_metadata.full_name;
+    },
+  },
+
   mutations: {
+    // alteran al estado al recibir info
+    setUser(state, user) {
+      state.userData = user;
+    },
   },
+
   actions: {
+    async getCurrentUser(context) {
+      const user = await supabaseClient.auth.user();
+      context.commit("setUser", user);
+      return user;
+    },
+
+    async doLogout(context) {
+      await supabaseClient.auth.signOut();
+      context.commit("setUser", null);
+    },
   },
-  modules: {
-  }
-})
+
+  modules: {},
+});
