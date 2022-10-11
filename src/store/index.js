0c329495c69546bcbbad34315a4368b1f3baf4a4
VickyMastro <vicmastroianni1702@gmail.com>
2022-10-11 15:11:50 -0300

    add modules to store (incomesModule)

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { supabaseClient } from "@/supabase/Client.js";
+import incomesModule from "@/store/modules/incomesModule.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     userData: null,
-    incomes: [],
   },
 
   getters: {
@@ -18,29 +18,12 @@ export default new Vuex.Store({
     getFullNameUser(state) {
       return state.userData.user_metadata.full_name;
     },
-
-    incomesOrderByDate(state) {
-      let orderByDate = {};
-      state.incomes.forEach((income) => {
-        // eslint-disable-next-line no-prototype-builtins
-        if(orderByDate.hasOwnProperty(income.date)){
-          orderByDate[income.date].push(income)
-        } else{
-          orderByDate[income.date] = [income]
-        }
-      });
-      return orderByDate;
-    },
   },
-  
+
   mutations: {
     // alteran al estado al recibir info
     setUser(state, user) {
       state.userData = user;
-    },
-
-    setIncome(state, income) {
-      state.incomes = income;
     },
   },
 
@@ -55,55 +38,9 @@ export default new Vuex.Store({
       await supabaseClient.auth.signOut();
       context.commit("setUser", null);
     },
-
-    async getIncomes(context) {
-      const incomes = await supabaseClient
-        .from("movements")
-        .select("*")
-        .eq("type", "income")
-        .order("date", {ascending: false});
-      context.commit("setIncome", incomes.data);
-    },
-
-    async getIncome(context, incomeId) {
-      const income = await supabaseClient
-        .from("movements")
-        .select("*")
-        .eq("id", incomeId);
-      return income.data[0];
-    },
-
-    async createIncome(context, incomeData) {
-      incomeData.user_id = context.state.userData.id;
-      incomeData.type = "income";
-
-      const { error } = await supabaseClient
-        .from("movements")
-        .insert([incomeData]);
-
-      if (error) throw error;
-    },
-
-    async editIncome(context,{ incomeData, id}){
-      const { error } = await supabaseClient
-        .from("movements")
-        .update([incomeData])
-        .match({id: id});
-
-      if (error) throw error;
-    },
-
-    async deleteIncome(context, incomeId){
-      const { error } = await supabaseClient
-        .from("movements")
-        .delete()
-        .match({id: incomeId});
-
-      if (error) throw error;
-
-      context.dispatch("getIncomes");
-    }
   },
 
-  modules: {},
+  modules: {
+    incomesModule,
+  },
 });
---
+++ src/store/modules/incomesModule.js
@@ -0,0 +1,74 @@
+import { supabaseClient } from "@/supabase/Client.js";
+export default {
+  state: {
+    incomes: [],
+  },
+  getters: {
+    incomesOrderByDate(state) {
+      let orderByDate = {};
+      state.incomes.forEach((income) => {
+        // eslint-disable-next-line no-prototype-builtins
+        if (orderByDate.hasOwnProperty(income.date)) {
+          orderByDate[income.date].push(income);
+        } else {
+          orderByDate[income.date] = [income];
+        }
+      });
+      return orderByDate;
+    },
+  },
+  mutations: {
+    setIncome(state, income) {
+      state.incomes = income;
+    },
+  },
+  actions: {
+    async getIncomes(context) {
+      const incomes = await supabaseClient
+        .from("movements")
+        .select("*")
+        .eq("type", "income")
+        .order("date", { ascending: false });
+      context.commit("setIncome", incomes.data);
+    },
+
+    async getIncome(context, incomeId) {
+      const income = await supabaseClient
+        .from("movements")
+        .select("*")
+        .eq("id", incomeId);
+      return income.data[0];
+    },
+
+    async createIncome(context, incomeData) {
+      incomeData.user_id = context.state.userData.id;
+      incomeData.type = "income";
+
+      const { error } = await supabaseClient
+        .from("movements")
+        .insert([incomeData]);
+
+      if (error) throw error;
+    },
+
+    async editIncome(context, { incomeData, id }) {
+      const { error } = await supabaseClient
+        .from("movements")
+        .update([incomeData])
+        .match({ id: id });
+
+      if (error) throw error;
+    },
+
+    async deleteIncome(context, incomeId) {
+      const { error } = await supabaseClient
+        .from("movements")
+        .delete()
+        .match({ id: incomeId });
+
+      if (error) throw error;
+
+      context.dispatch("getIncomes");
+    },
+  },
+};
