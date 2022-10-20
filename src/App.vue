59e8e41fc3f30bdca7a982d5bd6356487de5b4e7
VickyMastro <vicmastroianni1702@gmail.com>
2022-10-20 20:25:46 -0300

    add sass for change bootstrap-vue themes

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,10 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import './scss/custom.scss';
+@import "~bootstrap/scss/bootstrap.scss";
+@import "~bootstrap-vue/dist/bootstrap-vue.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -8,9 +8,6 @@ Vue.config.productionTip = false;
 // Bootstrap Vue
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
