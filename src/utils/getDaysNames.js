c17e62e8373c4cadf49706d42a3543b020532d56
VickyMastro <vicmastroianni1702@gmail.com>
2022-11-07 15:47:44 -0300

    number of day and name of week

--- src/utils/getDaysNames.js
+++ src/utils/getDaysNames.js
@@ -1,5 +1,13 @@
 export function getDaysNames(dateStr) {
-  let date = new Date(dateStr);
+  let dateSplit = dateStr.split("-");
+  let date = new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2]);
+
   let dayName = date.toLocaleDateString("es-AR", { weekday: "long" });
-  return dayName.charAt(0).toUpperCase() + dayName.slice(1) + ", " + dateStr.slice(-2);
+
+  return (
+    dayName.charAt(0).toUpperCase() +
+    dayName.slice(1) +
+    ", " +
+    dateStr.slice(-2)
+  );
 }
