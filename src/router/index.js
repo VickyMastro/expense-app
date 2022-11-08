d9400b335d2b3e5c6162cde40c4f3ad0bdd45c99
VickyMastro <vicmastroianni1702@gmail.com>
2022-11-08 19:41:29 -0300

    add route to disabled cashboxes

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import store from "../store"
+import store from "@/store";
 
-import CreateIncome from "../components/Incomes/CreateIncome"
-import EditIncome from "../components/Incomes/EditIncome"
+import CreateIncome from "@/components/Incomes/CreateIncome";
+import EditIncome from "@/components/Incomes/EditIncome";
 
-import CreateSpending from "../components/Spendings/CreateSpending"
-import EditSpending from "../components/Spendings/EditSpending"
+import CreateSpending from "@/components/Spendings/CreateSpending";
+import EditSpending from "@/components/Spendings/EditSpending";
 
-import Login from "../views/Login"
-import Movements from "../views/Movements"
+import Login from "@/views/Login";
+import Movements from "@/views/Movements";
+import DisabledCashboxes from "@/components/cashboxes/DisabledCashboxes"
 
 Vue.use(VueRouter);
 
-const checkUser = (async (to, from, next) => {
+const checkUser = async (to, from, next) => {
+  console.log("estoy en router");
   if (await store.dispatch("getCurrentUser")) {
-    next(); 
-  }
+    next();
+  } else {
   /* Si requiere auth y no esta logueado */
-  else {
-    next({name: 'Login'});
+    next({ name: "Login" });
   }
-});
+};
 
 const routes = [
   {
@@ -57,6 +58,12 @@ const routes = [
     path: "/:id/editSpending",
     name: "EditSpending",
     component: EditSpending,
+    beforeEnter: checkUser,
+  },
+  {
+    path: "/disabledCashboxes",
+    name: "DisabledCashboxes",
+    component: DisabledCashboxes,
     beforeEnter: checkUser,
   },
 ];
